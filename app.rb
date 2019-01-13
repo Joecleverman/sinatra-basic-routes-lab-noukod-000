a147d4529debe88eb0cdf885f5729af44cd895e8
Josue Abraham <josuea@noukod.com>
2019-01-13 08:55:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Joe"
+  end
+
+  get '/hometown' do
+    "My hometown is Petion Ville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Only you"
+  end
 end
